1a1eaec7d343ff8890d78a36b8f32b6e7b65443e
Jonathan Nilsson <jonathan.j.nilsson@gmail.com>
2018-11-04 23:06:00 +0100

    color changes, contact design

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
 import styles from './contact.module.css';
+import l from '../../images/l.svg';
 
 export const Contact = () => (
   <div className={styles.container}>
-    <h1 className={styles.h1}>Kontakta mig</h1>
-    <p>
-      Jag kan enklast nås på antingen <b>jonathan.j.nilsson@gmail.com</b> eller <p>LinkedIn</p>
-    </p>
+    <div className={styles.t1}><h1 className={styles.h1}>
+      <span className={styles.span} />
+      {'Kontakta mig'}
+    </h1>
+    </div>
+    <div className={styles.t3}>
+    <p className={styles.p1}>Apparently we had reached a great height in the atmosphere, for the sky was a dead black</p>
+    <p className={styles.p2}>Maila mig på</p>
+    <p className={styles.p3}>jonathan.j.nilsson@gmail.com</p>
+    </div>
+    <div>
+      <p className={styles.p2}>eller över</p>
+      <img src={l} />
+    </div>
   </div>
 );
